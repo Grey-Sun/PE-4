795e24784ad5069a816dc1d1e48beb89f03750b9
Grayson Arstingstall <grar6985@colorado.edu>
2021-09-24 16:43:27 +0000

    Completed the GetPlayerChoice function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -23,6 +23,23 @@ std::vector<std::vector<std::string>> CreateBoard(){
     return board;
 }
 
+std::vector<int> GetPlayerChoice(std::vector<std::vector<std::string>> board) {
+    while (true){ //set infinite condition to keep looping
+        std::vector<int> coordinate;
+        std::cout << "Please enter the column coordinate for your move (0-2): ";//prompt user for column
+        int column;
+        std::cin >> column; //get column
+        std::cout << "Please enter the row coordinate for your move (0-2): "; //prompt user for row input
+        int row;
+        std::cin >> row; //get row
+        if (board[column][row] == "."){ //make sure that the corresponding coordinate is valid
+            coordinate.push_back(row); //if so add the coordinate to the vector
+            coordinate.push_back(column);
+            return coordinate; //exit the loop
+        }
+    }
+}
+
 int main(){
     std::vector<std::vector<std::string>> game_board = CreateBoard();
     DisplayBoard(game_board);
